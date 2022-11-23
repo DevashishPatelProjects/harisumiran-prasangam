e986bc4cfe430c17fd8f698e82ba4826453f6e7a
Devashish Patel <dev.patel8787@gmail.com>
2022-11-23 15:19:55 -0500

    strapi initial setup

---
+++ packages/cms/src/api/parampara/controllers/parampara.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * parampara controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::parampara.parampara');
---
+++ packages/cms/src/api/parampara/routes/parampara.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * parampara router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::parampara.parampara');
